a30c690ea3425836ce61bff75145bf798d4ec9a1
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-06-24 09:28:38 +0530

    Create 01 First program.cpp

---
+++ 01 C++ Basics/01 First program.cpp
@@ -0,0 +1,8 @@
+// C with Classes is called C++ Programming Language.
+//C++ is a new version of C Programming Language.
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"Hello World.\nMy name is Aditya Raj. I am learning C++ programming Language from basic to Advanced with examples and building projects.";
+    return 0;
+}

53ebc5af4984fdb266eeb26be3bd7eff8c58faf5
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-18 23:08:02 +0530

    Create 03 data_type.cpp

---
+++ 01 C++ Basics/03 data_type.cpp
@@ -0,0 +1,21 @@
+//Everything is called data.
+//There are Many types of Data:-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age=20;
+    float percentage=85.5;
+    char grade='A';
+    double salary = 50000.75;
+    cout<<"Age="<<age;
+    cout<<"\nPercentage="<<percentage;
+    cout<<"\nGrade="<<grade;
+    cout<<"\nSalary="<<salary;
+    return 0;
+}
+//int → Integer values
+//float → Decimal values
+//char → single character
+//double → Large decimal values
+//string is not possible in c++ language without array

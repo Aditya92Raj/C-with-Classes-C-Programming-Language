69b74bacfcbfa2d1702951a82752c7ebf27086a7
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-18 23:19:57 +0530

    Create 01 arithmetic.cpp

---
+++ 02 Operators/01 arithmetic.cpp
@@ -0,0 +1,18 @@
+//Operator in C++ language:-
+//Arithmetic Operator(+,-,*,/,%)
+#include <iostream>
+using namespace std;
+int main() {
+    int a=9,b=3,Sum,difference,multiply;
+    Sum=a+b;
+    difference=a-b;
+    multiply=a*b;
+    cout<<"a="<<a;
+    cout<<"\nb="<<b;
+    cout<<"\nSum="<<Sum;
+    cout<<"\ndifference="<<difference;
+    cout<<"\nmultiply="<<multiply;
+    return 0;
+}
+//power is special case in c++ language not use '**' like python.
+//math.h library is used for power function.

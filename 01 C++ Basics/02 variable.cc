3af2edbc68e00505f0f3bab2fb4aa823f38d711a
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-18 22:06:07 +0530

    Create 02 variable.cc

---
+++ 01 C++ Basics/02 variable.cc
@@ -0,0 +1,15 @@
+//Variable is a container that stores various types of data in program.
+#include<iostream>
+using namespace std;
+int main()
+{
+    char Name='A';
+    int Age=20;
+    cout<<"Name in character form="<<Name;
+    cout<<"\nAge="<<Age;
+}
+//Variable can not be starts from number.so "1Name" is not true but "Name1" is always true.
+
+//space is not include in variable name.so "Na me" is not true but "Name" is true.
+
+//we cannot use special symbol or inverted commas for variable name.so "Name@12" is not true

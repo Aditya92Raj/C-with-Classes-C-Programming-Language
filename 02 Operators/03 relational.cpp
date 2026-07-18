5824fc6f3fd199a718e9c9233bb8d94b129a766b
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-18 23:31:40 +0530

    Create 03 relational.cpp

---
+++ 02 Operators/03 relational.cpp
@@ -0,0 +1,14 @@
+//Operator in C++ language:-
+//Relational Operator(==,!=,<=,>=,<,>)
+#include <iostream>
+using namespace std;
+int main() {
+    int a=5,b=22;
+    cout<<(a>=b);
+    cout<<"\n"<<(a<=b);
+    cout<<"\n"<<(a<b);
+    cout<<"\n"<<(a>b);
+    return 0;
+}
+//relational operator is used to compare the value and result or output is always present as Boolean (0 and 1).
+//If condition is true output will 1 and if condition is false output will 0.

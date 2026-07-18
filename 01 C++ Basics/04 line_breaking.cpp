172c4c5f625f9687b1ca8de48a27d9931e7fbf3a
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-18 23:11:40 +0530

    Create 04 line_breaking.cpp

---
+++ 01 C++ Basics/04 line_breaking.cpp
@@ -0,0 +1,8 @@
+//Line Break in c++.
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"My name is Aditya Raj.\nI am from Vaishali District.\nCurrently I am pursuing BCA from OCRM Patna, Bihar.";
+}
+//Any data or values cannot start from new line automatically in C++. It is only possible with the help of line break ("\n").

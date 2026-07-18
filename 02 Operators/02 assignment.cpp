ba6698ad0049fdf4117dde47b3b07d6710e57fec
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-18 23:25:22 +0530

    Create 02 assignment.cpp

---
+++ 02 Operators/02 assignment.cpp
@@ -0,0 +1,17 @@
+//Operator in C++ language:-
+//Assignment Operator(+=,-=,*=,/=,%=)
+#include <iostream>
+using namespace std;
+int main() {
+    int a=5,b=22;
+    cout<<"before assignment";
+    cout<<"\na="<<a;
+    cout<<"\nb="<<b;
+    a+=15;
+    b-=10;
+    cout<<"\nafter assignment";
+    cout<<"\na="<<a;
+    cout<<"\nb="<<b;
+    return 0;
+}
+//assignment operator is used to assing value and change the value

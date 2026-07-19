fc62923beb8fb6aad3be0393a81a8d4782a01b15
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-19 23:51:56 +0530

    Create 09 find_the_smallest_from_three_numbers.cpp

---
+++ 03 Conditional Statements/09 find_the_smallest_from_three_numbers.cpp
@@ -0,0 +1,22 @@
+// Write a program to input three number from user find the smallest of three numbers.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter three numbers:";
+    cin>>a>>b>>c;
+    if (a<=b && a<=c)
+    {
+        cout<<a<<" is the smallest.";
+    }
+    else if (b<=a && b<=c)
+    {
+        cout<<b<<" is the smallest.";
+    }
+    else
+    {
+        cout<<c<<" is the smallest.";
+    }
+    return 0;
+}

3ed759bbd0aa2000f5c3bea286708cd8185b0a8d
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-21 23:35:38 +0530

    Create 03 print_numbers_from_50 to 1.cpp

---
+++ 05 Loops/02 for loop/03 print_numbers_from_50 to 1.cpp
@@ -0,0 +1,11 @@
+//write a program to print numbers in reverse order from 50 to 1 using for loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=50;i>=1;i--)
+    {
+        cout<<i<<"\n";
+    }
+    return 0;
+}

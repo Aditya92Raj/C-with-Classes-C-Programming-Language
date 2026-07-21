9601db4c9ea39c8a78311b1e5c6c40dafd18e249
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-21 23:29:27 +0530

    Create 01 basic_for_loop_program.cpp

---
+++ 05 Loops/02 for loop/01 basic_for_loop_program.cpp
@@ -0,0 +1,12 @@
+//A for loop is commonly used when the number of iterations is known.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for (i=1;i<=10;i++)
+    {
+        cout<<i<<"\n";
+    }
+    return 0;
+}

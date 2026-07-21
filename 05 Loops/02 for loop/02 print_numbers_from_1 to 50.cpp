0e6644b41edb03d77d00644a579474b445c64a28
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-21 23:33:01 +0530

    Create 02 print_numbers_from_1 to 50.cpp

---
+++ 05 Loops/02 for loop/02 print_numbers_from_1 to 50.cpp
@@ -0,0 +1,11 @@
+//write a program to print numbers from 1 to 50 using for loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=50;i++)
+    {
+        cout<<i<<"\n";
+    }
+    return 0;
+}

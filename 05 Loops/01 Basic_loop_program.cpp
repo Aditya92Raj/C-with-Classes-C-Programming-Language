b9a3c16ae7a65067bf29794ca0d797d3e82fccb8
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-21 23:25:23 +0530

    Create 01 Basic_loop_program.cpp

---
+++ 05 Loops/01 Basic_loop_program.cpp
@@ -0,0 +1,19 @@
+//A loop is a programming statement that allows a block of code to execute repeatedly until a specified condition becomes false. Loops help avoid writing the same code multiple times.
+
+//there are three types of loops:-
+//(i)for loop
+//(ii)while loop
+//(iii)do-while loop
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    while (i<=5)
+    {
+        cout<<i<<"\n";
+        i++;
+    }
+    return 0;
+}

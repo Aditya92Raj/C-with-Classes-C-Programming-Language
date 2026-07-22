999e17547430888973e399a92ddae35a3b7dfd45
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-22 23:50:02 +0530

    Create 04 print_square_of_numbers_from_1 to 10.cpp

---
+++ 05 Loops/03 while loop/04 print_square_of_numbers_from_1 to 10.cpp
@@ -0,0 +1,13 @@
+//write a program to print square of numbers from 1 to 10 using while loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    while (i<=10)
+    {
+        cout<<i*i<<"\n";
+        i++;
+    }
+    return 0;
+}

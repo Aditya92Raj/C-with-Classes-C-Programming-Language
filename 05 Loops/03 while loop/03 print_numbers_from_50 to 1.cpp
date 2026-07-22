41d1b65f8c5b5e964b9f7064e5f8b3fb464ca18a
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-22 23:46:45 +0530

    Create 03 print_numbers_from_50 to 1.cpp

---
+++ 05 Loops/03 while loop/03 print_numbers_from_50 to 1.cpp
@@ -0,0 +1,13 @@
+//write a program to print reverse numbers from 50 to 1 using while loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=50;
+    while (i>0)
+    {
+        cout<<i<<"\n";
+        i--;
+    }
+    return 0;
+}

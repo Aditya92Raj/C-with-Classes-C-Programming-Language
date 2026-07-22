b877f0aac00f4ac13870d8dfb82de3b951b817f2
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-23 00:13:34 +0530

    Create 05 cube_of_numbers_from_1 to 10.cpp

---
+++ 05 Loops/04 do-while loop/05 cube_of_numbers_from_1 to 10.cpp
@@ -0,0 +1,13 @@
+//write a program to print cube numbers from 1 to 10 using do-while loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    do
+    {
+        cout<<i*i*i<<"\n";
+        i++;
+    } while (i<=10);
+    return 0;
+}

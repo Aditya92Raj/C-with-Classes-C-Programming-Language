1acb81bc65826f9a0312324297bda7da8c814590
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-22 23:56:12 +0530

    Create 02 print_numbers_from_1 to 50.cpp

---
+++ 05 Loops/04 do-while loop/02 print_numbers_from_1 to 50.cpp
@@ -0,0 +1,13 @@
+//write a program to print numbers from 1 to 50 using do-while loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    do
+    {
+        cout<<i<<"\n";
+        i++;
+    } while (i<=50);
+    return 0;
+}

d6a12f226f2280232776af2192c5386511c480bb
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-23 00:11:11 +0530

    Create 03 print_numbers_from_50 to 1.cpp

---
+++ 05 Loops/04 do-while loop/03 print_numbers_from_50 to 1.cpp
@@ -0,0 +1,13 @@
+//write a program to print numbers from 50 to 1 using do-while loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=50;
+    do
+    {
+        cout<<i<<"\n";
+        i--;
+    } while (i>0);
+    return 0;
+}

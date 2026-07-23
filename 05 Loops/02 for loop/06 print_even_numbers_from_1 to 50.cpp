13e65f05133c5bb1be75378991605026ac1ecc23
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-23 22:59:26 +0530

    Create 06 print_even_numbers_from_1 to 50.cpp

---
+++ 05 Loops/02 for loop/06 print_even_numbers_from_1 to 50.cpp
@@ -0,0 +1,11 @@
+//write a program to print even numbers from 1 to 50 using for loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=2;i<=50;i+=2)
+    {
+        cout<<i<<"\n";
+    }
+    return 0;
+}

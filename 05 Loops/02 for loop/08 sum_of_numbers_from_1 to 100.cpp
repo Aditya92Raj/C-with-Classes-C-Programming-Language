e83cffcb6fc27519bcbf53d8cf1c5df82654be4f
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-23 23:04:20 +0530

    Create 08 sum_of_numbers_from_1 to 100.cpp

---
+++ 05 Loops/02 for loop/08 sum_of_numbers_from_1 to 100.cpp
@@ -0,0 +1,13 @@
+//write a program to print sum of numbers from 1 to 50 using for loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=0;
+    for(int i=1;i<=100;i++)
+    {
+        a+=i;
+    }
+    cout<<a;
+    return 0;
+}

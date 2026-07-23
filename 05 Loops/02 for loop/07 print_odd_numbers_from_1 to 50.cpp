592d1cbd179bb8f27d49f29ef92ba96de2f4d1a1
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-23 23:00:38 +0530

    Create 07 print_odd_numbers_from_1 to 50.cpp

---
+++ 05 Loops/02 for loop/07 print_odd_numbers_from_1 to 50.cpp
@@ -0,0 +1,11 @@
+//write a program to print odd numbers from 1 to 50 using for loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=50;i+=2)
+    {
+        cout<<i<<"\n";
+    }
+    return 0;
+}

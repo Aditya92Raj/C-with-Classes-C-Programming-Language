0fb8cbd001efb6fdd300a5143bcd4657a63b8ab7
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-24 23:27:14 +0530

    Create 16 lowercase_letters_from_a_to_z.cpp

---
+++ 05 Loops/02 for loop/16 lowercase_letters_from_a_to_z.cpp
@@ -0,0 +1,12 @@
+//Write a program to print lowercase letters from a to z.
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    for(ch='a';ch<='z';ch++)
+    {
+        cout<<ch<<"\n";
+    }
+    return 0;
+}

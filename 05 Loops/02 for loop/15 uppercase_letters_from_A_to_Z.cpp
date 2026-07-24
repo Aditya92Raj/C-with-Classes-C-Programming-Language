eef62c941e71cb311495ad57b1b828308dc3f75b
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-24 23:25:34 +0530

    Create 15 uppercase_letters_from_A_to_Z.cpp

---
+++ 05 Loops/02 for loop/15 uppercase_letters_from_A_to_Z.cpp
@@ -0,0 +1,12 @@
+//Write a program to print uppercase letters from A to Z.
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    for(ch='A';ch<='Z';ch++)
+    {
+        cout<<ch<<"\n";
+    }
+    return 0;
+}

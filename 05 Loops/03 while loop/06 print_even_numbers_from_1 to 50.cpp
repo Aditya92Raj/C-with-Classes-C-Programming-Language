952eb931038ccf6ab3a32108220b5dd78c9fcfc0
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-25 23:42:33 +0530

    Create 06 print_even_numbers_from_1 to 50.cpp

---
+++ 05 Loops/03 while loop/06 print_even_numbers_from_1 to 50.cpp
@@ -0,0 +1,13 @@
+//write a program to print even numbers from 1 to 50 using while loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=2;
+    while(i<=50)
+    {
+        cout<<i<<"\n";
+        i+=2;
+    }
+    return 0;
+}

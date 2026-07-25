7679ee2e8df321aa2846078aaed92dc97b6c8fb8
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-25 23:43:53 +0530

    Create 07 print_odd_numbers_from_1 to 50.cpp

---
+++ 05 Loops/03 while loop/07 print_odd_numbers_from_1 to 50.cpp
@@ -0,0 +1,13 @@
+//write a program to print odd numbers from 1 to 50 using while loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    while(i<=50)
+    {
+        cout<<i<<"\n";
+        i+=2;
+    }
+    return 0;
+}

bf668ef08b6f8c0faf4961566e1bda2d01e815b8
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-27 00:05:45 +0530

    Create 09 sum_of_even_numbers_from_1 to 100.cpp

---
+++ 05 Loops/04 do-while loop/09 sum_of_even_numbers_from_1 to 100.cpp
@@ -0,0 +1,14 @@
+//write a program to print sum of even numbers from 1 to 100 using do-while loop in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=2,sum=0;
+    while (i<=100)
+    {
+        sum=sum+i;
+        i+=2;
+    }
+    cout<<"Sum of even numbers="<<sum;
+    return 0;
+}

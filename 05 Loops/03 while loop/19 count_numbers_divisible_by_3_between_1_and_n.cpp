0b708c51e514f677638938722b2cb0d6597ab32d
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-27 22:46:40 +0530

    Create 19 count_numbers_divisible_by_3_between_1_and_n.cpp

---
+++ 05 Loops/03 while loop/19 count_numbers_divisible_by_3_between_1_and_n.cpp
@@ -0,0 +1,19 @@
+//Write a program to count numbers divisible by 3 between 1 and N.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,count,i=1;
+    cout<<"Enter a number:";
+    cin>>a;
+    while(i<=a)
+    {
+        if(i%3==0)
+        {
+            count++;
+        }
+        i++;
+    }
+    cout<<"Total numbers whose divisible by 3 = "<<count;
+    return 0;
+}

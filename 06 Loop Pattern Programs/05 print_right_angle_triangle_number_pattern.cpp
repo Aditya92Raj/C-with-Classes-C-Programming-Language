0ab983cdd7b4eddc5614ab3fe7d7cb85cc51903f
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-07-31 00:05:12 +0530

    Create 05 print_right_angle_triangle_number_pattern.cpp

---
+++ 06 Loop Pattern Programs/05 print_right_angle_triangle_number_pattern.cpp
@@ -0,0 +1,17 @@
+//write a program to print a Right angle triangle number pattern in c++ language.
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cout<<"Enter a number:";
+    cin>>a;
+    for(int i=1;i<=a;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<j;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}

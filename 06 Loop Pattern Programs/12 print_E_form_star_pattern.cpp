56ab1e36a69acada72758ca8e934dbc3652236b0
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-08-02 23:09:10 +0530

    Create 12 print_E_form_star_pattern.cpp

---
+++ 06 Loop Pattern Programs/12 print_E_form_star_pattern.cpp
@@ -0,0 +1,22 @@
+//write a program to print E form star pattern in c++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+
+    for(int i=1;i<=n;i++)
+    {
+        for (int j=1;j<=n;j++)
+        {
+            if (j==1 || i==1 || i==n || i==n/2+1)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}

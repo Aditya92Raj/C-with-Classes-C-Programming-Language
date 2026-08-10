7b2e44aee5895841c6b8f9c0105446a0a433bdab
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-08-10 23:12:04 +0530

    Create 19 print_d_form_star_pattern.cpp

---
+++ 06 Loop Pattern Programs/19 print_d_form_star_pattern.cpp
@@ -0,0 +1,26 @@
+//Write a program to print b form star pattern in C++ language.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,i,j;
+    cout<<"Enter a number:";
+    cin>>a;
+    for(i=1;i<=a;i++)
+    {
+        for(j=1;j<=a;j++)
+        {
+            if(j==a || (i==a/2+1 && j!=1) || (i==a && j!=1) || (j==1 && i>a/2+1 && i<a))
+            
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}

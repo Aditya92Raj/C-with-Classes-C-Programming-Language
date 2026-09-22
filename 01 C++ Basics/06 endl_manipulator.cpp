7da6510c3878bd7bd41c960598f048fa44652dce
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-22 23:21:07 +0530

    Create 06 endl_manipulator.cpp

---
+++ 01 C++ Basics/06 endl_manipulator.cpp
@@ -0,0 +1,8 @@
+//endl is a manipulator that inserts a new line and flushes the output buffer.
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<"My name is Aditya Raj."<<endl<<"I am from Bihar.";
+return 0;
+}

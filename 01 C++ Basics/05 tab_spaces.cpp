97b841de4e8f47d39c4b56e1eb3b04aef1b807f2
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-22 23:07:15 +0530

    Create 05 tab_spaces.cpp

---
+++ 01 C++ Basics/05 tab_spaces.cpp
@@ -0,0 +1,10 @@
+//Multiple spaces with the help of Tab
+#include<stdio.h>
+int main()
+{
+printf("Name:\tAditya Raj");
+printf("\nAge:\t18");
+printf("\nGender:\tMale");
+printf("\nState:\tBihar");
+}
+//tab space in c++ is used to write multiple spaces at a time

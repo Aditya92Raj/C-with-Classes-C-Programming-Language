edb96e4428c97f3c086fc51084759b6d6efd97c4
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-23 23:57:46 +0530

    Create 05 sizeof_operator.cpp

---
+++ 02 Operators/05 sizeof_operator.cpp
@@ -0,0 +1,9 @@
+//sizeof is a unari operator in C++ used to determine the size of a data type or object in bytes.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    cout<<sizeof(a);
+    return 0;
+}

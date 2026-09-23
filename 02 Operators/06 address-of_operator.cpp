302a025e41fff7bb9b7da08eb606bd0a5931ae9a
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-24 00:02:28 +0530

    Create 06 address-of_operator.cpp

---
+++ 02 Operators/06 address-of_operator.cpp
@@ -0,0 +1,10 @@
+// & is called the address-of operator when it is used with a variable. It gives the memory address of that variable.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    cout<<a<<endl;
+    cout<<&a<<endl;
+    return 0;
+}

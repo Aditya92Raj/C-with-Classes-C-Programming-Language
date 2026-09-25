e66ebbd0641ef032f079547de536281a145cf29d
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-25 22:14:17 +0530

    Create 02 post-increment_operator.cpp

---
+++ 02 Operators/07 Increment operator/02 post-increment_operator.cpp
@@ -0,0 +1,11 @@
+//Operator in C++ language:-
+//post-increment operator(a++)
+#include <iostream>
+using namespace std;
+int main() {
+    int a=5;
+    cout<<a++<<endl;
+    cout<<a;
+return 0;
+}
+//The post-increment operator uses the current value of a variable first and then increase it by 1.

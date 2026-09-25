4c47d555c4959c9e32fb128b4087532da87b963d
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-25 22:08:47 +0530

    Create 01 pre-increment_operator.cpp

---
+++ 02 Operators/07 Increment operator/01 pre-increment_operator.cpp
@@ -0,0 +1,11 @@
+//Operator in C++ language:-
+//pre-increment operator(++a)
+#include <iostream>
+using namespace std;
+int main() {
+    int a=5;
+    cout<<++a<<endl;
+    cout<<a;
+return 0;
+}
+//The pre-increment operator increases the value of a variable by 1 before using its value in an expression

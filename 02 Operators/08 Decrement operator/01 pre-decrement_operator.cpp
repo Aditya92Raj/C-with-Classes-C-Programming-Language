718a0c3c850743b0fb5eff593d054de76ce85ee3
Aditya Raj <adityarajadityaraj893@gmail.com>
2026-09-25 22:20:31 +0530

    Create 01 pre-decrement_operator.cpp

---
+++ 02 Operators/08 Decrement operator/01 pre-decrement_operator.cpp
@@ -0,0 +1,11 @@
+//Operator in C++ language:-
+//pre-Decrement operator(--a)
+#include <iostream>
+using namespace std;
+int main() {
+    int a=5;
+    cout<<--a<<endl;
+    cout<<a;
+return 0;
+}
+//The pre-decrement operator decrease the value of a variable by 1 before using its value in an expression.
